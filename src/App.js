60151969856bb5505e52643df72d4f580a9c2eed
soosap <prasath.soosaithasan@gmail.com>
2016-09-25 18:40:34 +0200

    Add styling to root level <App /> component

--- src/App.js
+++ src/App.js
@@ -1,14 +1,58 @@
 import React, { Component } from 'react';
-import { Text, View } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
 
 class App extends Component {
 	render() {
 		return (
-			<View>
-				<Text>GithubNotes</Text>
+			<View style={styles.container}>
+				<Text>GithubNotes Body Content</Text>
 			</View>
 		);
 	}
 }
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		padding: 30,
+		marginTop: 65,
+		flexDirection: 'column',
+		justifyContent: 'center',
+		backgroundColor: '#48bbec',
+	},
+	title: {
+		marginBottom: 20,
+		fontSize: 25,
+		textAlign: 'center',
+		color: '#fff'
+	},
+	search: {
+		height: 50,
+		padding: 4,
+		marginRight: 5,
+		fontSize: 23,
+		borderWidth: 1,
+		borderColor: 'white',
+		borderRadius: 8,
+		color: 'white',
+	},
+	buttonText: {
+		fontSize: 18,
+		color: '#111',
+		alignSelf: 'center',
+	},
+	button: {
+		height: 45,
+		flexDirection: 'row',
+		backgroundColor: 'white',
+		borderColor: 'white',
+		borderWidth: 1,
+		borderRadius: 8,
+		marginBottom: 10,
+		marginTop: 10,
+		alignSelf: 'stretch',
+		justifyContent: 'center',
+	},
+});
 
 export default App;
