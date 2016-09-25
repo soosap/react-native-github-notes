a3b7328f23a8897103cdc11ff820964c23e7903e
soosap <prasath.soosaithasan@gmail.com>
2016-09-25 18:41:35 +0200

    Make use of rn's <NavigatorIOS /> component for navigation

--- index.ios.js
+++ index.ios.js
@@ -1,4 +1,26 @@
-import { AppRegistry } from 'react-native';
+import React, { Component } from 'react';
+import { AppRegistry, NavigatorIOS, StyleSheet } from 'react-native';
 import App from './src/App';
 
-AppRegistry.registerComponent('GithubNotes', () => App);
+class GithubNotes extends Component {
+	render() {
+		return (
+			<NavigatorIOS
+				initialRoute={{
+					title: 'Github Notes',
+					component: App
+				}}
+				style={styles.container}
+			/>
+		);
+	}
+}
+
+const styles = StyleSheet.create({
+	container: {
+		flex: 1,
+		backgroundColor: '#111111',
+	},
+});
+
+AppRegistry.registerComponent('GithubNotes', () => GithubNotes);
