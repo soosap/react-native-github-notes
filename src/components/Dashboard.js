ed63258b51af8c746a532f8519855ee7f39e0f52
soosap <prasath.soosaithasan@gmail.com>
2016-09-26 15:18:24 +0200

    Add Github user's avatar to <Dashboard /> component

--- src/components/Dashboard.js
+++ src/components/Dashboard.js
@@ -1,10 +1,18 @@
 import React, { Component } from 'react';
-import { Text, View, StyleSheet } from 'react-native';
+import {
+	Text,
+	View,
+	Image,
+	TouchableHighlight,
+	StyleSheet,
+} from 'react-native';
 
 class Dashboard extends Component {
 	render() {
+		console.log('this.props.userInfo: ', this.props.userInfo);
 		return (
 			<View style={styles.container}>
+				<Image source={{ uri: this.props.userInfo.avatar_url }} style={styles.image} />
 				<Text>Dashboard</Text>
 			</View>
 		);
