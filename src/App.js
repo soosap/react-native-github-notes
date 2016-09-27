72fdfdb54d20d4fdecc22da199348b2b0094a8b4
soosap <prasath.soosaithasan@gmail.com>
2016-09-27 19:07:11 +0200

    Initialize firebase in <App />'s componentWillMount lifecycle hook

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
 import React, { Component, PropTypes } from 'react';
 import { View, StyleSheet } from 'react-native';
+import firebase from 'firebase';
 
 import SearchForm from './components/SearchForm';
 
 class App extends Component {
 	static propTypes = {
 		navigator: PropTypes.object.isRequired,
+	}
+	
+	componentWillMount() {
+		firebase.initializeApp({
+			apiKey: 'AIXaSyCxtXPd_WOoX6VAGxow4lIK-bsZPX5vXxN8QXd',
+			authDomain: 'react-native-fbauth.firebaseapp.com',
+			databaseURL: 'https://react-native-fbauth.firebaseio.com',
+			storageBucket: 'react-native-fbauth.appspot.com',
+			messagingSenderId: '345333254237'
+		});
 	}
 
 	render() {
