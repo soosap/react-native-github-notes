8797f4e6adb5779fc8721f07cc803b8ee2e13334
soosap <prasath.soosaithasan@gmail.com>
2016-09-27 19:05:11 +0200

    Add "getNotes" and "addNote" methods to persist data w/ firebase

--- src/utils/api/github.js
+++ src/utils/api/github.js
@@ -1,4 +1,5 @@
 import axios from 'axios';
+import firebase from 'firebase';
 
 export default {
 	getBio(username) {
@@ -9,4 +10,18 @@ export default {
 		const query = username.toLowerCase().trim();
 		return axios.get(`https://api.github.com/users/${query}/repos`);
 	},
+	getNotes(username) {
+		const usernameX = username.toLowerCase().trim();
+		return firebase.database().ref(`/notes/${usernameX}`).once('value');
+	},
+	addNote(username, note) {
+		const usernameX = username.toLowerCase().trim();
+		const newPostKey = firebase.database().ref().child('notes').push().key;
+
+		const updates = {};
+		updates[`/notes/${newPostKey}`] = { body: note };
+		updates[`/notes/${usernameX}/${newPostKey}`] = { body: note };
+
+		return firebase.database().ref().update(updates);
+	}
 };
